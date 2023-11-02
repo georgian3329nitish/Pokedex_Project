6d83825b6bfa356f951070390bb68cad89374c2d
georgian3329nitish <nitishkumar332921@gmail.com>
2023-11-02 12:45:26 +0530

    added routing

--- src/components/Pokedex/Pokedex.jsx
+++ src/components/Pokedex/Pokedex.jsx
@@ -7,11 +7,10 @@ function Pokedex(){
 
 return(
     <div className="pokedex-wrapper">
-       <h1 id="pokedex-heading">Pokedex</h1>
       <Search />
       <PokemonList />
     </div>
-)
+);
 
 }
 export default Pokedex;
